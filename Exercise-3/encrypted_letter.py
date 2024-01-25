b8b04503b09a48c1a274e3c99351034b06466ab8
ArianaS83 <157647518+ArianaS83@users.noreply.github.com>
2024-01-26 01:55:20 +0330

    refactored and cleaned version

--- Exercise-3/encrypted_letter.py
+++ Exercise-3/encrypted_letter.py
@@ -1,5 +1,13 @@
-txt = str(input())
-ans = txt.split(' ')
-ans.sort(key=lambda x: int(x[1:]))
-for i in ans :
-    print(i[0],end='')
+def sort_and_print_first_char(txt):
+    words = txt.split(' ')
+    sorted_words = sorted(words, key=lambda x: int(x[1:]))
+    
+    for word in sorted_words:
+        print(word[0], end='')
+
+def main():
+    user_input = str(input("Enter space-separated words: "))
+    sort_and_print_first_char(user_input)
+
+if __name__ == "__main__":
+    main()
